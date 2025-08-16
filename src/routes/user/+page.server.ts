0b5262458081f8ff4137ff0723a9ef3043d33df3
dai-rewahandi <niktodaire@gmail.com>
2025-08-16 21:51:32 +0700

    add logic to add llink

--- src/routes/user/+page.server.ts
+++ src/routes/user/+page.server.ts
@@ -1,8 +1,8 @@
-import { redirect, type Cookies } from "@sveltejs/kit";
+import { fail, redirect, type Cookies } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { db } from "$lib/server/db";
 import { eq } from "drizzle-orm";
-import { sessions_db, user_db } from "$lib/server/db/schema";
+import { link_db, sessions_db, user_db } from "$lib/server/db/schema";
 
 export const load: PageServerLoad = async ({ cookies }: { cookies: Cookies }) => {
 	const cookies_check = cookies.get('sessions_id')
@@ -18,4 +18,27 @@ export const load: PageServerLoad = async ({ cookies }: { cookies: Cookies }) =>
 	return {
 		data: user_data
 	}
+}
+
+
+export const actions = {
+				addlink: async ({ request, cookies }:{ request: Request, cookies: Cookies }) => {
+								const form = await request.formData()
+								const name = form.get('name')
+								const link = form.get('link')
+								if(!name || typeof name !== 'string' || !link || typeof link !== 'string') return fail(400, { mode: 'error', message: 'data faild!' })
+								try{
+												const cookies_check = cookies.get('sessions_id')
+												if(!cookies_check) return ;
+												const [sessions_check] = await db.select().from(sessions_db).where(eq(sessions_db.sessions_token, cookies_check))
+												await db.insert(link_db).values({
+																name,
+																link,
+																user_id: sessions_check.user_id
+												})
+												return { mode: 'success', message: 'New link added!' }
+								}catch(error){
+												return fail(400, { mode: 'error', message: 'Add faild!' })
+								}
+				}
 }
--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,7 +1,12 @@
 <script>
+	import Toast from '$lib/components/Toast.svelte';
 	import { Avatar, Button, Card, Input } from 'flowbite-svelte';
-	let { data } = $props();
+	let { data, form } = $props();
 </script>
+
+{#if form?.message}
+	<Toast mode={form.mode} message={form.message} />
+{/if}
 
 <div class="container grid h-full w-full grow gap-4 md:grid-cols-2">
 	<div class="flex flex-col gap-4">
@@ -14,9 +19,9 @@
 		<Card class="p-4 md:p-6 xl:p-8">
 			<form class="flex flex-col items-center gap-4">
 				<h3 class="font-semibold">Add Link</h3>
-				<Input name="link" id="link" placeholder="https://" required />
+				<Input name="link" id="link" placeholder="https://" value='https://' required />
 				<Input name="name" id="name" placeholder="name" required />
-				<Button class="btn w-full">add</Button>
+				<Button class="btn w-full" type='submit' formaction='?/addlink' formmethod='post'>add</Button>
 			</form>
 		</Card>
 	</div>
