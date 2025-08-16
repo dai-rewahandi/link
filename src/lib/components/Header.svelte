17235c86591faad2624caadc0eaa5d003cb881fb
dai-rewahandi <niktodaire@gmail.com>
2025-08-16 20:11:23 +0700

    add image
    add image if user login

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,24 @@
 <script>
-	import { Navbar, NavBrand, Button } from 'flowbite-svelte';
+	import { Navbar, NavBrand, Button, Avatar } from 'flowbite-svelte';
 	export let is_signin = false;
 </script>
 
 <Navbar>
 	<NavBrand href="/" class="text-xl font-bold">LnkDB</NavBrand>
 	{#if is_signin}
-	<form>
-		<Button type='submit' formaction='/signin/?/signout' formmethod='post' class='pointer-events-auto cursor-pointer'>SignOut</Button>
-	</form>
+		<div class="flex items-center gap-4">
+			<a href="/user">
+				<Avatar src="https://dummyimage.com/600x600/000/fff" alt="Avatar" />
+			</a>
+			<form>
+				<Button
+					type="submit"
+					formaction="/signin/?/signout"
+					formmethod="post"
+					class="pointer-events-auto cursor-pointer">SignOut</Button
+				>
+			</form>
+		</div>
 	{:else}
 		<Button href="/signup">SignUp / SignIn</Button>
 	{/if}
