dcc43690d6a358672add9b43df7b92c089f35f0f
dai-rewahandi <niktodaire@gmail.com>
2025-08-16 20:14:57 +0700

    a

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,22 +1,22 @@
 import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
 
 export const user_db = sqliteTable('user_db', {
-	id: integer('id').primaryKey(),
-	name: text('name').notNull(),
-	username: text('username').unique().notNull(),
-	email: text('email').notNull().unique(),
-	password: text('password').notNull()
+        id: integer('id').primaryKey(),
+        name: text('name').notNull(),
+        username: text('username').unique().notNull(),
+        email: text('email').notNull().unique(),
+        password: text('password').notNull()
 });
 
 export const sessions_db = sqliteTable('sessions_db', {
-	id: integer('id').primaryKey(),
-	sessions_token: text('sessions_token').notNull(),
-	user_id: integer('user_id').notNull().references(() => user_db.id, { onDelete: 'cascade' })
+        id: integer('id').primaryKey(),
+        sessions_token: text('sessions_token').notNull(),
+        user_id: integer('user_id').notNull().references(() => user_db.id, { onDelete: 'cascade' })
 })
 
 export const link_db = sqliteTable('link_db', {
-				id: integer('id').primaryKey(),
-				name: text('name').notNull(),
-				link: text('link').notNull(),
-				user_id: integer('user_id').notNull().references(() => user_db.id, { onDelete: 'cascade' })
+        id: integer('id').primaryKey(),
+        name: text('name').notNull(),
+        link: text('link').notNull(),
+        user_id: integer('user_id').notNull().references(() => user_db.id, { onDelete: 'cascade' })
 })
