58fc74ce88c5e70a1c163613c59e6c11c4762ebe
dai-rewahandi <niktodaire@gmail.com>
2025-08-16 21:26:01 +0700

    add form add-link

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,20 +1,22 @@
 <script>
-	import { Avatar, Card, Input } from 'flowbite-svelte';
+	import { Avatar, Button, Card, Input } from 'flowbite-svelte';
 	let { data } = $props();
 </script>
 
 <div class="container grid h-full w-full grow gap-4 md:grid-cols-2">
-	<div>
+	<div class="flex flex-col gap-4">
 		<Card class="flex items-center p-4 pb-4 md:p-6 xl:p-8">
 			<Avatar size="lg" class="mb-2" src="https://dummyimage.com/1080x1080/000/fff" alt="Avatar" />
 			<h5 class="text-lg font-semibold">{data.data?.name}</h5>
 			<span class="font-light text-gray-600">{data.data?.email}</span>
 		</Card>
 
-		<Card>
-			<form>
-				<Input name='link' id='link' placeholder='https://' required/>
-				<Input name='name' id='name' placeholder='name' required/>
+		<Card class="p-4 md:p-6 xl:p-8">
+			<form class="flex flex-col items-center gap-4">
+				<h3 class="font-semibold">Add Link</h3>
+				<Input name="link" id="link" placeholder="https://" required />
+				<Input name="name" id="name" placeholder="name" required />
+				<Button class="btn w-full">add</Button>
 			</form>
 		</Card>
 	</div>
