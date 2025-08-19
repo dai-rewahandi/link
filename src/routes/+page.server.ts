3033cc6ac630dd6e9cf483cee51eec3cca65e2f2
dai-rewahandi <niktodaire@gmail.com>
2025-08-19 21:00:28 +0700

    get all username

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,9 +4,10 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const links = await db.select().from(link_db);
-	const users = await db.select().from(user_db);
+	const users = await db.select({ username: user_db.username, rule: user_db.rule }).from(user_db);
 	return {
 		data: {
+            allusername: users ,
 			alllink: links.length,
 			alluser: users.length
 		}
