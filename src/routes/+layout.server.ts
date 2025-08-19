0106f4f530093255449fe2b9e4c8f86bc3d10f2f
dai-rewahandi <niktodaire@gmail.com>
2025-08-19 21:00:40 +0700

    pretiier

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,18 +5,18 @@ import { sessions_db, user_db } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
 
 export const load: PageServerLoad = async ({ cookies }: { cookies: Cookies }) => {
-	const cookies_check = cookies.get('sessions_id');
-	if (!cookies_check) return { is_signin: false };
-	const [sessions_check] = await db
-		.select()
-		.from(sessions_db)
-		.where(eq(sessions_db.sessions_token, cookies_check));
-	if (!sessions_check) return { is_signin: false };
-const [user] = await db.select().from(user_db).where(eq(user_db.id, sessions_check.user_id))
-	return { 
-								data: { 
-												is_signin: true, 
-												user: user.username 
-								}
-				};
+    const cookies_check = cookies.get('sessions_id');
+    if (!cookies_check) return { is_signin: false };
+    const [sessions_check] = await db
+        .select()
+        .from(sessions_db)
+        .where(eq(sessions_db.sessions_token, cookies_check));
+    if (!sessions_check) return { is_signin: false };
+    const [user] = await db.select().from(user_db).where(eq(user_db.id, sessions_check.user_id))
+    return {
+        data: {
+            is_signin: true,
+            user: user.username
+        }
+    };
 };
