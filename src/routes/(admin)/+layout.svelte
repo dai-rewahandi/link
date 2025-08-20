feef87e31d7c70e8accf819878c9d5224f2db054
dai-rewahandi <niktodaire@gmail.com>
2025-08-20 12:49:33 +0700

    refactor: Reorganize routes into (admin) and (user) groups
    This commit refactors the SvelteKit routing structure by introducing two route groups: `(admin)` and `(user)`. This change improves code organization by separating administrative routes from public-facing user routes.
    
    Key changes:
    
    - Moved all administrative pages (signin, signup, user settings) into the `src/routes/(admin)` directory.
    
    - Created a new `src/routes/(user)` directory for user-facing pages, including the public user profile page.
    
    - Added a new root layout for the `(user)` group.
    
    - Updated import paths and URL construction in the layouts to reflect the new structure.
    
    - Added new user-uploaded images to be tracked.

--- src/routes/(admin)/+layout.svelte
+++ src/routes/(admin)/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
-	import '../app.css';
+	import '../../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/state';
 
 	let { children, data } = $props();
+    let base = page.url.host
 </script>
 
 <svelte:head>
@@ -17,9 +19,8 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<!-- NOTE: avatar libk in ver mode oonly -->
 <main class="flex min-h-screen w-screen flex-col justify-between">
-	<Header is_signin={data.data?.is_signin} avatar={`http://localhost:5173/uploads/${data.data?.user}.jpeg`}/>
+	<Header is_signin={data.data?.is_signin} avatar={'http://' + base + '/uploads/' + data.data?.user + '.jpeg'}/>
 	<section class="flex grow flex-col items-center p-4">
 		{@render children?.()}
 	</section>
---
+++ src/routes/(user)/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<main class="flex items-center justify-center w-screen h-screen">
+	{@render children()}
+    <footer class="absolute bottom-2 left-2 ">
+        <p><a href="/">Home</a> | <a href="/signup">signup</a> | &copy; dai 2025</p>
+    </footer>
+</main>
